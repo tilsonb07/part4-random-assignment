38973a9babef4d2d446a3352ab614c5b1729ab6c
Tilson Beattie <BeatTils377@GoTVDSB.ca>
2020-09-29 09:54:40 -0400

    done

--- part4-random assignment/Form1.cs
+++ part4-random assignment/Form1.cs
@@ -15,17 +15,17 @@ namespace part4_random_assignment
         Random generator = new Random();
 
 
-        
+
         public frm1()
         {
             InitializeComponent();
         }
 
-       
 
-        
 
-        
+
+
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -47,7 +47,7 @@ namespace part4_random_assignment
 
         }
 
-        
+
 
         private void txtboxmin_TextChanged(object sender, EventArgs e)
         {
@@ -57,17 +57,51 @@ namespace part4_random_assignment
         private void btnSubmit_Click_1(object sender, EventArgs e)
         {
             int max = Convert.ToInt32(txtboxmax.Text);
-            int min = Convert.ToInt32(txtboxmin.Text); 
-       
+            int min = Convert.ToInt32(txtboxmin.Text);
+
             lblNumber.Text = generator.Next(min, max + 1) + "";
         }
 
         private void btnDouble_Click(object sender, EventArgs e)
         {
-        //    double min = Conv
-        //    double max = Conv
-        //    double randomDouble = generator.NextDouble() * (max - min) + min;
-        //    lblNumber.Text = randomDouble.ToString();
-        //}
+
+            int max = Convert.ToInt32(txtboxmax.Text);
+            int min = Convert.ToInt32(txtboxmin.Text);
+
+            lblNumber.Text = generator.NextDouble() + generator.Next(min, max) + "";
+            
+
+
+
+
+
+
+
+
+           
+
+
+
+            
+
+
+           
+
+
+
+
+            
+
+
+
+
+
+
+
+        }
     }
 }
+
+
+
+
